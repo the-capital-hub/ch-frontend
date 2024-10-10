d76aec00f61409ebce95b5814a61cdb2e76d3655
Capital Hub <dev.capitalhub@gmail.com>
2024-10-10 16:32:57 +0530

    email update of investor

--- src/components/Shared/InvestorProfileComponent/InvestorProfileList.jsx
+++ src/components/Shared/InvestorProfileComponent/InvestorProfileList.jsx
@@ -3,7 +3,6 @@ import InvestorProfile from "./InvestorProfile";
 
 
 export default function InvestorProfileList({ theme, short, data }) {
-  console.log("INVESTOR", data[1].isInvestor)
   return (
     <div className="d-flex flex-column gap-3">
       {data?.map((person) => {
