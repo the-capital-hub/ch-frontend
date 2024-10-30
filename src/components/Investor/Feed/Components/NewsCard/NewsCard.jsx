22a194b1ed2d35038409836dad7cdf090cdd493c
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2024-10-30 11:21:34 +0530

    News Corner

--- src/components/Investor/Feed/Components/NewsCard/NewsCard.jsx
+++ src/components/Investor/Feed/Components/NewsCard/NewsCard.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import './NewsCard.scss'; 
+import React from "react";
+import "./NewsCard.scss";
+import Newscard from "../../../../../Images/Newscard.svg";
+import dummyImageUrl from "../../../../../Images/blog/1 AsPGU1Q42C9lsVRoMg91Nw.webp";
 
 const NewsCard = ({ title, description, url, urlToImage, publishedAt }) => {
-    return (
-        <div className="news-card">
-            <img src={urlToImage} alt={title} className="news-card__image" />
-            <h3 className="news-card__title">{title}</h3>
-            <p className="news-card__description">{description}</p>
-            <a href={url} target="_blank" rel="noopener noreferrer" className="news-card__link">
-                Read more
-            </a>
-            {/* <span className="news-card__date">{new Date(publishedAt).toLocaleDateString()}</span> */}
-        </div>
-    );
+	return (
+		<div className="news-card">
+			{/* <div className="news-card-tag">News</div> */}
+			<img
+				src={Newscard}
+				alt="News Tag"
+				className="news-card-tag"
+				style={{ width: "80px" }}
+			/>
+			<img
+				src={urlToImage || dummyImageUrl}
+				alt={title}
+				onError={(e) => {
+					e.target.onerror = null; // Prevents infinite loop in case the dummy image also fails
+					e.target.src = dummyImageUrl; // Set to dummy image if the original fails
+				}}
+				className="news-card__image"
+			/>
+			<h3 className="news-card__title">{title}</h3>
+			<p className="news-card__description">{description}</p>
+			<a
+				href={url}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="news-card__link"
+			>
+				Read more
+			</a>
+			{/* <span className="news-card__date">{new Date(publishedAt).toLocaleDateString()}</span> */}
+		</div>
+	);
 };
 
 export default NewsCard;
