d70d73dc3eda2168a82c5cbe36f216b8424e2c09
Capital Hub <dev.capitalhub@gmail.com>
2024-11-26 16:34:46 +0530

    new env added

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,8 +1,10 @@
 export const environment = {
 	production: false,
 	//baseUrl: "https://thecapitalhub.onrender.com",
-	baseUrl: "http://localhost:8080",
+	// baseUrl: "http://localhost:8080",
 	//  baseUrl: "https://api.thecapitalhub.in",
+	//  baseUrl: "http://api.thecapitalhub.in",
+	 baseUrl: "http://ec2-13-233-61-13.ap-south-1.compute.amazonaws.com:8080",
 };
 
 // export const LINKDIN = "https://www.linkedin.com/developers/apps/verification2c20194f-b5dd-4fe3-a1ce-715dd9aeb301"
