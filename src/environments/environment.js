3d52662c4f2405ed1c4be12bf5325b0530145880
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2024-12-27 19:37:06 +0530

    fix

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,9 +1,8 @@
 export const environment = {
 	production: false,
 	//baseUrl: "https://thecapitalhub.onrender.com",
-	baseUrl: "http://localhost:8080",
-	  // baseUrl: "https://api.thecapitalhub.in",
-	
+	// baseUrl: "http://localhost:8080",
+	baseUrl: "https://api.thecapitalhub.in",
 };
 
 // export const LINKDIN = "https://www.linkedin.com/developers/apps/verification2c20194f-b5dd-4fe3-a1ce-715dd9aeb301"
