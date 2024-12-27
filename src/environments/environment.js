0af0a6d481d649be70680858e8f83021e51d03cd
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2024-12-27 19:34:56 +0530

    onelink profile page and feedpostcard commentcount badge

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,8 +1,8 @@
 export const environment = {
 	production: false,
 	//baseUrl: "https://thecapitalhub.onrender.com",
-	//baseUrl: "http://localhost:8080",
-	  baseUrl: "https://api.thecapitalhub.in",
+	baseUrl: "http://localhost:8080",
+	  // baseUrl: "https://api.thecapitalhub.in",
 	
 };
 
