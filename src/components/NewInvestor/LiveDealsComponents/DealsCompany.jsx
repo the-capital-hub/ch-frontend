5800ac2cbda63deaf4112475b3f78c2dba5ab029
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2025-01-31 16:33:09 +0530

    DealsCompany

--- src/components/NewInvestor/LiveDealsComponents/DealsCompany.jsx
+++ src/components/NewInvestor/LiveDealsComponents/DealsCompany.jsx
@@ -17,14 +17,15 @@ import {
 import { useEffect, useState } from "react";
 import RevenueStatistics from "../../../components/NewInvestor/LiveDealsComponents/RevenueStatistics";
 import CurrentFunding from "./CurrentFunding";
+import { useNavigate } from "react-router-dom";
 
 export default function DealsCompany({ company, setData }) {
-  const theme = useSelector(selectTheme);
-  const navigate = useNavigate();
-  const loggedInUserId = useSelector(selectLoggedInUserId);
-  const isInvestor = useSelector(selectIsInvestor);
-  const [userInterested, setUserInterested] = useState([]);
-  const [founderId, setFounderId] = useState({});
+	const theme = useSelector(selectTheme);
+	const navigate = useNavigate();
+	const loggedInUserId = useSelector(selectLoggedInUserId);
+	const isInvestor = useSelector(selectIsInvestor);
+	const [userInterested, setUserInterested] = useState([]);
+	const [founderId, setFounderId] = useState({});
 
 	// Extract founderId safely
 
@@ -68,40 +69,39 @@ export default function DealsCompany({ company, setData }) {
 		}
 	};
 
-  // return (
-  //   <div
-  //     className="company__deals shadow-sm border rounded-4"
-  //     style={{
-  //       background: theme === "dark" ? "#212224" : "#f5f5f5",
-  //       padding: "2rem",
-  //     }}
-  //   >
-  //     <DealsHeader
-  //       image={company.startupId.logo}
-  //       name={company.startupId.company}
-  //       motto={company.startupId.sector}
-  //       theme={theme}
-  //       handelDeals={handelDeals}
-  //       loggedInUserId={loggedInUserId}
-  //       userInterested={userInterested}
-  //       companyId={companyId}
-  //       founderId={founderId}
-  //       isInvestor={isInvestor}
-  //     />
-  //     <DealsOverview
-  //       name={company.name}
-  //       about={company.startupId.description}
-  //       theme={theme}
-  //     />
-  //     <DealsInvestors
-  //       theme={theme}
-  //       intrustedInvestor={company.intrustedInvestor}
-  //     />
-  //     {/* <DealsFunds theme={theme} /> */}
-  //     <RevenueStatistics/>
-      
-  //     <CurrentFunding/>
+	return (
+		<div
+			className="company__deals shadow-sm border rounded-4"
+			style={{
+				background: theme === "dark" ? "#212224" : "#f5f5f5",
+				padding: "2rem",
+			}}
+		>
+			<DealsHeader
+				image={company.startupId.logo}
+				name={company.startupId.company}
+				motto={company.startupId.sector}
+				theme={theme}
+				handelDeals={handelDeals}
+				loggedInUserId={loggedInUserId}
+				userInterested={userInterested}
+				companyId={companyId}
+				founderId={founderId}
+				isInvestor={isInvestor}
+			/>
+			<DealsOverview
+				name={company.name}
+				about={company.startupId.description}
+				theme={theme}
+			/>
+			<DealsInvestors
+				theme={theme}
+				intrustedInvestor={company.intrustedInvestor}
+			/>
+			{/* <DealsFunds theme={theme} /> */}
+			<RevenueStatistics />
 
-    </div>
-  );
+			<CurrentFunding />
+		</div>
+	);
 }
