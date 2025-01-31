88205e1b3a9afbd3e69684275edbd65f9247d61a
Vishal Mishra <vm174294@gmail.com>
2025-01-31 13:46:10 +0530

    Minor-ui-fixed

--- src/components/NewInvestor/LiveDealsComponents/DealsCompany.jsx
+++ src/components/NewInvestor/LiveDealsComponents/DealsCompany.jsx
@@ -14,6 +14,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 export default function DealsCompany({ company, setData }) {
   const theme = useSelector(selectTheme);
+  const navigate = useNavigate();
   const loggedInUserId = useSelector(selectLoggedInUserId);
   const isInvestor = useSelector(selectIsInvestor);
   const [userInterested, setUserInterested] = useState([]);
@@ -61,39 +62,39 @@ export default function DealsCompany({ company, setData }) {
     }
   };
 
-  return (
-    <div
-      className="company__deals shadow-sm border rounded-4"
-      style={{
-        background: theme === "dark" ? "#212224" : "#f5f5f5",
-        padding: "2rem",
-      }}
-    >
-      <DealsHeader
-        image={company.startupId.logo}
-        name={company.startupId.company}
-        motto={company.startupId.sector}
-        theme={theme}
-        handelDeals={handelDeals}
-        loggedInUserId={loggedInUserId}
-        userInterested={userInterested}
-        companyId={companyId}
-        founderId={founderId}
-        isInvestor={isInvestor}
-      />
-      <DealsOverview
-        name={company.name}
-        about={company.startupId.description}
-        theme={theme}
-      />
-      <DealsInvestors
-        theme={theme}
-        intrustedInvestor={company.intrustedInvestor}
-      />
-      {/* <DealsFunds theme={theme} /> */}
-      <RevenueStatistics/>
+  // return (
+  //   <div
+  //     className="company__deals shadow-sm border rounded-4"
+  //     style={{
+  //       background: theme === "dark" ? "#212224" : "#f5f5f5",
+  //       padding: "2rem",
+  //     }}
+  //   >
+  //     <DealsHeader
+  //       image={company.startupId.logo}
+  //       name={company.startupId.company}
+  //       motto={company.startupId.sector}
+  //       theme={theme}
+  //       handelDeals={handelDeals}
+  //       loggedInUserId={loggedInUserId}
+  //       userInterested={userInterested}
+  //       companyId={companyId}
+  //       founderId={founderId}
+  //       isInvestor={isInvestor}
+  //     />
+  //     <DealsOverview
+  //       name={company.name}
+  //       about={company.startupId.description}
+  //       theme={theme}
+  //     />
+  //     <DealsInvestors
+  //       theme={theme}
+  //       intrustedInvestor={company.intrustedInvestor}
+  //     />
+  //     {/* <DealsFunds theme={theme} /> */}
+  //     <RevenueStatistics/>
       
-      <CurrentFunding/>
+  //     <CurrentFunding/>
 
   console.log("company", company);
 
